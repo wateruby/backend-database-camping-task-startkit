a636428082c5e42a06202ff44f38cb7b861673d4
Wateruby <kellytu57@gmail.com>
2024-12-05 17:30:35 +0800

    TASK1-4

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -123,6 +123,7 @@ INSERT INTO "COURSE"(user_id,skill_id,name,start_at,end_at,max_participants,meet
 ((SELECT id FROM "USER" WHERE name='李燕容'),(SELECT id FROM "SKILL" WHERE name='重訓'),'重訓基礎課','2024-11-25 14:00:00','2024-11-25 16:00:00',10,'https://test-meeting.test.io');
 
 
+
 -- ████████  █████   █    █████ 
 --   █ █   ██    █  █     █     
 --   █ █████ ███ ███      ████  
